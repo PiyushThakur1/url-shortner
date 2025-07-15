6c333815fdc6ef82ee56a620f393cdce0db53f63
Piyush Singh <piyusshthakur5@gmail.com>
2025-07-15 17:19:15 +0530

    refactor: replace sync file methods with async/await

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { nanoid } = require("nanoid");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const port = 3000;
 
@@ -11,38 +11,59 @@ app.use(express.json());
 
 const file = path.join(__dirname, "..", "data", "urls.json");
 
-function readUrlFile() {
-  if (!fs.existsSync(file)) {
+// function readUrlFile() {
+//   if (!fs.existsSync(file)) {
+//     return {};
+//   }
+//   const jsonData = fs.readFileSync(file, "utf-8");
+//   return JSON.parse(jsonData);
+// }
+
+async function readUrlFile() {
+  try {
+    await fs.access(file);
+    const jsonData = await fs.readFile(file, "utf-8");
+    return JSON.parse(jsonData);
+  } catch (err) {
     return {};
   }
-  const jsonData = fs.readFileSync(file, "utf-8");
-  return JSON.parse(jsonData);
 }
 
-function writeUrlFile(data) {
+// function writeUrlFile(data) {
+//   const jsonData = JSON.stringify(data, null, 2);
+//   fs.writeFileSync(file, jsonData, "utf-8");
+// }
+
+async function writeUrlFile(data) {
   const jsonData = JSON.stringify(data, null, 2);
-  fs.writeFileSync(file, jsonData, "utf-8");
+  await fs.writeFile(file, jsonData, "utf-8");
 }
 
-app.post("/shorten", (req, res) => {
-  const { url } = req.body;
+app.post("/shorten", async (req, res) => {
+  try {
+    const { url } = req.body;
 
-  if (!url || typeof url !== "string") {
-    return res.status(400).json({ message: "Invalid or Missing originalUrl" });
+    if (!url || typeof url !== "string") {
+      return res
+        .status(400)
+        .json({ message: "Invalid or Missing originalUrl" });
+    }
+
+    const shortId = nanoid(6);
+    const shortUrl = `http://localhost:${port}/${shortId}`;
+
+    const urlData = await readUrlFile();
+    urlData[shortId] = url;
+    await writeUrlFile(urlData);
+
+    res.status(201).json({
+      url,
+      shortUrl,
+      shortId,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong" });
   }
-
-  const shortId = nanoid(6);
-  const shortUrl = `http://localhost:${port}/${shortId}`;
-
-  const urlData = readUrlFile();
-  urlData[shortId] = url;
-  writeUrlFile(urlData);
-
-  res.status(201).json({
-    url,
-    shortUrl,
-    shortId,
-  });
 });
 
 app.listen(port, () => {
